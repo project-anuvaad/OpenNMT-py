2320832dc6230c10fdad83c26558fc0892b7a8b5
aj7tesh <ajitesh.sharma@tarento.com>
2019-12-12 20:46:23 +0530

    exp-5.8 incremental training

--- anuvaad_scripts.py
+++ anuvaad_scripts.py
@@ -153,6 +153,54 @@ def english_hindi_experiments():
         print(e)
         logger.info("error in english_hindi anuvaad script: {}".format(e))
 
+def incremental_training(src_sp_model,tgt_sp_model,train_from_model):
+    try:
+        model_intermediate_folder = os.path.join(INTERMEDIATE_DATA_LOCATION, 'english_hindi')
+        model_master_train_folder = os.path.join(TRAIN_DEV_TEST_DATA_LOCATION, 'english_hindi')
+        nmt_model_path = os.path.join(NMT_MODEL_DIR, 'english_hindi','model_en-hi_inc_exp-5.8_{}-model'.format(date_now))
+        if not any([os.path.exists(model_intermediate_folder),os.path.exists(model_master_train_folder),os.path.exists(os.path.join(NMT_MODEL_DIR, 'english_hindi'))]):
+            os.makedirs(model_intermediate_folder)
+            os.makedirs(model_master_train_folder)
+            os.makedirs(os.path.join(NMT_MODEL_DIR, 'english_hindi'))
+            print("folder created at {}".format(model_intermediate_folder))
+        hindi_tokenized_file = os.path.join(model_intermediate_folder, 'hindi_train_tok.txt')
+        hindi_dev_tokenized_file = os.path.join(model_intermediate_folder, 'hindi_dev_tok.txt')
+        english_tokenized_file = os.path.join(model_intermediate_folder, 'english_train_tok.txt')
+        english_dev_tokenized_file = os.path.join(model_intermediate_folder, 'english_dev_tok.txt')
+        hindi_encoded_file = os.path.join(model_master_train_folder, 'hindi_train_final.txt')
+        hindi_dev_encoded_file = os.path.join(model_master_train_folder, 'hindi_dev_final.txt')
+        english_encoded_file = os.path.join(model_master_train_folder, 'english_train_final.txt')
+        english_dev_encoded_file = os.path.join(model_master_train_folder, 'english_dev_final.txt')
+        nmt_processed_data = os.path.join(model_master_train_folder, 'processed_data_inc_exp-5.8_{}'.format(date_now))
+
+        print("Exp-5.8 incremental training")
+        print("src sp:{},tgt sp:{}, from:{}".format(src_sp_model,tgt_sp_model,train_from_model))
+        os.system('python ./tools/indic_tokenize.py {0} {1} hi'.format(mcl.english_hindi['HINDI_TRAIN_FILE'], hindi_tokenized_file))
+        os.system('python ./tools/indic_tokenize.py {0} {1} hi'.format(mcl.english_hindi['DEV_HINDI'], hindi_dev_tokenized_file))
+        logger.info("english-hindi, hindi train and dev corpus tokenized")
+        os.system('perl ./tools/tokenizer.perl <{0}> {1}'.format(mcl.english_hindi['ENGLISH_TRAIN_FILE'], english_tokenized_file))
+        os.system('perl ./tools/tokenizer.perl <{0}> {1}'.format(mcl.english_hindi['DEV_ENGLISH'], english_dev_tokenized_file))
+        logger.info("english-hindi, english train, dev,test corpus tokenized")
+        print("tokenization done")
+
+        sp.encode_as_pieces(tgt_sp_model,hindi_tokenized_file,hindi_encoded_file)
+        sp.encode_as_pieces(tgt_sp_model,hindi_dev_tokenized_file,hindi_dev_encoded_file)
+        logger.info("hindi-train file and dev encoded and final stored in data folder")
+        sp.encode_as_pieces(src_sp_model,english_tokenized_file,english_encoded_file)
+        sp.encode_as_pieces(src_sp_model,english_dev_tokenized_file,english_dev_encoded_file)
+        logger.info("english-train,dev,test file encoded and final stored in data folder")
+        print("english-train,dev,test file encoded and final stored in data folder")
+
+        os.system('python preprocess.py -train_src {0} -train_tgt {1} -valid_src {2} -valid_tgt {3} -src_seq_length 200 -tgt_seq_length 200 -save_data {4}'.format(english_encoded_file,hindi_encoded_file,english_dev_encoded_file,hindi_dev_encoded_file,nmt_processed_data))
+        print("preprocessing done")
+        os.system('python ./embeddings_to_torch.py -emb_file_enc ~/glove/glove.6B.300d.txt -emb_file_dec ~/glove/cc.hi.300.vec -dict_file {0} -output_file {1}'.format(nmt_processed_data+'.vocab.pt',os.path.join(model_master_train_folder,'embeddings_eng_hin')))
+        print("glove embedding done")
+        os.system('nohup python train.py -data {0} -save_model {1} -layers 6 -rnn_size 512 -word_vec_size 512 -pre_word_vecs_enc {2} -pre_word_vecs_dec {3} -transformer_ff 2048 -heads 8 -encoder_type transformer -decoder_type transformer -position_encoding -train_from {4} -train_steps 270000  -max_generator_batches 2 -dropout 0.1 -batch_size 6000 -batch_type tokens -normalization tokens  -accum_count 2 -optim adam -adam_beta2 0.998 -decay_method noam -warmup_steps 8000 -learning_rate 0.25 -max_grad_norm 0 -param_init 0  -param_init_glorot  -label_smoothing 0.1 -valid_steps 10000 -save_checkpoint_steps 10000 -world_size 1 -gpu_ranks 0'.format(nmt_processed_data,nmt_model_path,os.path.join(model_master_train_folder,'embeddings_eng_hin.enc.pt'),os.path.join(model_master_train_folder,'embeddings_eng_hin.dec.pt'),train_from_model))
+
+    except Exception as e:
+        print(e)
+        logger.info("error in english_hindi anuvaad script: {}".format(e))
+
 def english_hindi_experiments_word_based():
 
     "exp-4.Word based model + tokenization +6000 on 1gpu +all lowercasing"
@@ -750,6 +798,8 @@ if __name__ == '__main__':
     elif sys.argv[1] == "hinidi-english-exp":
         hindi_english_experiments()
     elif sys.argv[1] == "english-hindi-exp-sv":
-        english_hindi_sharevocab_experiments()                                            
+        english_hindi_sharevocab_experiments()
+    elif sys.argv[1] == "incremental_training":
+        incremental_training(sys.argv[2],sys.argv[3],sys.argv[4])                                                
     else:
         print("invalid request", sys.argv)
