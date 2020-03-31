29ee2bf1be35e2ec62cf43bf30c477cab4250f27
aj7tesh <ajitesh.sharma@tarento.com>
2020-03-31 17:08:01 +0530

    handled unk token format in the output

--- translation_util/translate_util.py
+++ translation_util/translate_util.py
@@ -489,6 +489,7 @@ def translate_func(inputs, translation_server):
                     raise Exception("unsupported model id: {} for given input".format(i['id']))      
 
                 # translation = (prefix+" "+translation+" "+suffix).strip()
+                translation = translation.replace("▁"," ")
                 translation = date_url_util.replace_tags_with_original_1(translation,date_original,url_original,num_array)
             logger.info("trans_function-experiment-{} output: {}".format(i['id'],translation))    
             tgt.append(translation)
