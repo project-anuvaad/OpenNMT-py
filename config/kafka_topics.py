8022eb51fd1542fdc5960f7abda14b7d73355672
aj7tesh <ajitesh.sharma@tarento.com>
2020-04-27 15:41:34 +0530

    kafka second-testing function

--- config/kafka_topics.py
+++ config/kafka_topics.py
@@ -1,9 +1,11 @@
 consumer_topics ={
   "TEST_TOPIC":"testtopic",
-  "DOCUMENT_REQ":"to-nmt"
+  "DOCUMENT_REQ":"to-nmt",
+  "new_topic":"nmt_translate"
 }
 
 producer_topics ={
   "TEST_TOPIC":"listener",
-  "TO_DOCUMENT":"listener"
+  "TO_DOCUMENT":"listener",
+  "new_topic":"nmt_translate_processed"
 }
