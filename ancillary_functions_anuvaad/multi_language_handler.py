dba0424c7bf45fe4bd8e06ddc0dd7bd7cc176ed0
aj7tesh <ajitesh.sharma@tarento.com>
2020-04-30 16:34:18 +0530

    added color demarcations in logs and multi language utility file

---
+++ ancillary_functions_anuvaad/multi_language_handler.py
@@ -0,0 +1,45 @@
+from textblob import TextBlob
+import re
+
+'''
+Currently only handling foreign words(english) during vernacular to english translation
+UuRrLl token not working correctly need to retrain the model with new tokens
+'''
+def tag_english_words(text):
+    try:
+        regex = r'[A-Za-z]+'
+        list1=re.findall(regex,text)
+        count_word = 0
+
+        for j in list1:
+            text = text.replace(str(j),'UuRrLl'+str(count_word),1)
+            count_word +=1
+        print(text)
+        print(list1)
+        return text,list1
+    except Exception as e:
+        print(e)
+
+def replace_english_words(text,word_arr):
+    try:      
+        if len(text) == 0:
+            return ""
+
+        word_tagged_arr = re.findall(r'UuRrLl..|UuRrLl.', text)   
+        for j in word_tagged_arr:
+            j = j.replace(' ','')
+            if j[:-1] == "UuRrLl":
+                text = text.replace(j,(word_arr[int(j[-1])]),1)
+            elif j[-2:].isnumeric():
+                text = text.replace(j,(word_arr[int(j[-2:])]),1) 
+            else:
+                text = text.replace(j[:-1],(word_arr[int(j[-2])]),1) 
+
+        print("response after url and date replacemnt:{}".format(text))
+        return text
+    except Exception as e:
+        print(e)
+
+# a,b = tag_english_words("பொன்னர்சங்கர் and கோ என்ற கூட்டாண்மை நிறுவனத்தை (Partnership Firm) நடத்தி வருகிறார்கள் என்றும்,")        
+# replace_english_words(a,b)
+# replace_english_words("பொன்னர்சங்கர் UuRrLl0 கோ என்ற கூட்டாண்மை நிறுவனத்தை (UuRrLl1 UuRrLl12) நடத்தி வருகிறார்கள் என்றும்,",['and', 'Partnership', 'Firm','and', 'Partnership', 'Firm','and', 'Partnership', 'Firm','and', 'Partnership', 'Firm','ajitesh'])
--- onmt/utils/logging.py
+++ onmt/utils/logging.py
@@ -2,17 +2,37 @@
 from __future__ import absolute_import
 
 import logging
+import colorama
+import copy
 
 logger = logging.getLogger()
 
+LOG_COLORS = {
+    logging.ERROR: colorama.Fore.RED,
+    logging.WARNING: colorama.Fore.YELLOW
+}
+'''
+Using different color logs for console output
+'''
+class ColorFormatter(logging.Formatter):
+    def format(self, record, *args, **kwargs):
+        new_record = copy.copy(record)
+        if new_record.levelno in LOG_COLORS:
+            new_record.levelname = "{color_begin}{level}{color_end}".format(
+                level=new_record.levelname,
+                color_begin=LOG_COLORS[new_record.levelno],
+                color_end=colorama.Style.RESET_ALL,
+            )
+        return super(ColorFormatter, self).format(new_record, *args, **kwargs)
 
 def init_logger(log_file=None, log_file_level=logging.NOTSET):
-    log_format = logging.Formatter("[%(asctime)s %(levelname)s] %(message)s")
+    log_format = logging.Formatter("[%(asctime)s %(levelname)s %(filename)s %(lineno)d] %(message)s")
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
 
+    formatter = ColorFormatter("[%(asctime)s %(levelname)s %(filename)s %(lineno)d] %(message)s")
     console_handler = logging.StreamHandler()
-    console_handler.setFormatter(log_format)
+    console_handler.setFormatter(formatter)
     logger.handlers = [console_handler]
 
     if log_file and log_file != '':
